4e9d1ba337e871ea04b6ee5c08e01a06c1a11951
Jeremy Andrews Zantua <tmq.jeremy.z@gmail.com>
2023-03-16 02:49:55 +0800

    Adds basic routing structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Import Pages
+
+// Import Components
+// Components go here
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
