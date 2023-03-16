5bc045f7a8828f0412abc2f8d1bb42dc60c6d692
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-03-16 22:55:34 +0800

    Implements hooks and auth context in login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { useLogin } from "../hooks/useLogin";
+import { useAuthContext } from "../hooks/useAuthContext";
 import { Link, Navigate } from "react-router-dom";
 import {
   Typography,
@@ -13,14 +15,16 @@ import {
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  // TODO Add hooks for login
-  // TODO Add hooks for auth context
+  const { login, error, message, isLoading } = useLogin();
+  const { user } = useAuthContext();
 
-  // TODO add check for redirecting to homepage if a user is already logged in
+  if (user) {
+    return <Navigate to="/" />;
+  }
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    // TODO Handle login using login hook
+    await login(email, password);
   };
 
   return (
