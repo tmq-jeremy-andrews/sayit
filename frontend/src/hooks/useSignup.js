8e596e9bc47197f3983dff509054de9ceaeea5f8
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-03-16 22:49:56 +0800

    Adds hooks

---
+++ frontend/src/hooks/useSignup.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export const useSignup = () => {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+  const [message, setMessage] = useState(null);
+
+  const signup = async (email, password, confirmPassword) => {
+    setIsLoading(true);
+    setError(null);
+
+    const response = await fetch("/api/users/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password, confirmPassword }),
+    });
+
+    const json = await response.json();
+
+    if (!response.ok) {
+      setIsLoading(false);
+      setError(json.error);
+      setMessage(null);
+    }
+
+    if (response.ok) {
+      setIsLoading(false);
+      setError(null);
+      setMessage(json.message);
+    }
+  };
+
+  return { signup, isLoading, error, message };
+};
