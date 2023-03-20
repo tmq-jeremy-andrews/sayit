1a8ebd2eb7a6081de253b609581032e30e432d36
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-03-20 23:12:07 +0800

    Updates private message model

--- backend/models/privateMessage.js
+++ backend/models/privateMessage.js
@@ -2,11 +2,12 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const privateMessageSchema = new Schema(
+const PrivateMessageSchema = new Schema(
   {
     chat: {
       type: Schema.Types.ObjectId,
       ref: "PrivateChat",
+      required: true,
     },
     content: {
       type: String,
@@ -16,21 +17,15 @@ const privateMessageSchema = new Schema(
       type: Schema.Types.ObjectId,
       ref: "User",
     },
-    read_by: [
-      {
-        user: {
-          type: Schema.Types.ObjectId,
-          ref: "User",
-          required: true,
-        },
-        time: {
-          type: Date,
-          required: true,
-        },
-      },
-    ],
+    received: {
+      type: Boolean,
+      default: false,
+    },
+    read: {
+      type: Date,
+    },
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("PrivateMessage", privateMessageSchema);
+module.exports = mongoose.model("PrivateMessage", PrivateMessageSchema);
